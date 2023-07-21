6c0058075e394abfc48fde691e48ea459c13f0b3
pundsahil09 <pundsahil09@gmail.com>
2023-07-21 10:55:34 +0530

    commit 3

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const http = require('http');
+// Creating http server on localhost:4500
+const http = require('http'); 
 const uc = require('uppercase');
 
 http.createServer((req,resp)=>{
-    resp.writeHead(200,{'content-type':'text/html'})
+    resp.writeHead(200,{'content-type':'text/html'}) // this is meta (plain/html)
     resp.write(uc("<h1> Hello Node Js </h1>"))
     resp.write(uc("<h1> hello </h1>"))
     resp.end();
